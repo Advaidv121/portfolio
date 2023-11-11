b7496fb3785ba118c18e5a04df9650dc1396bc0d
Advaid V <advaid121@gmail.com>
2023-11-11 11:56:05 +0530

    download button

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
-            // Add a click event listener to the body element
-            document.body.addEventListener('click', function () {
-                // Toggle the 'overflow-hidden' class on the body element
-                document.body.classList.toggle('overflow-hidden');
+    // Add a click event listener to the button element
+    document.getElementById('toggleButton').addEventListener('click', function () {
+        // Toggle the 'overflow-hidden' class on the body element
+        document.body.classList.toggle('overflow-hidden');
 
-                // Toggle the 'hidden' class on the aside element
-                const aside = document.querySelector('aside');
-                if (aside) {
-                    aside.classList.toggle('hidden');
-                }
-            });
-        });
+        // Toggle the 'hidden' class on the aside element
+        const aside = document.querySelector('aside');
+        if (aside) {
+            aside.classList.toggle('hidden');
+        }
+    });
+});

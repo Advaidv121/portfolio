2d8df1f0b66cc4296841c22e34450ae563f73476
Advaid V <advaid121@gmail.com>
2023-11-11 10:31:32 +0530

    last

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+            // Add a click event listener to the body element
+            document.body.addEventListener('click', function () {
+                // Toggle the 'overflow-hidden' class on the body element
+                document.body.classList.toggle('overflow-hidden');
+
+                // Toggle the 'hidden' class on the aside element
+                const aside = document.querySelector('aside');
+                if (aside) {
+                    aside.classList.toggle('hidden');
+                }
+            });
+        });
